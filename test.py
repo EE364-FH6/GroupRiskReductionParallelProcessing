646ade2e91e7bb03c1af640cddeb8f27372dcfc2
Abdullah <abdullah.haris507@gmail.com>
2021-11-17 19:25:56 -0600

    Updated

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ yt.set_filename('GeeksforGeeks Video')
  
 # get the video with the extension and
 # resolution passed in the get() function
-d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution)
+d_video = yt.streams.filter(progressive = True, file_extension = "mp4").first()
 try:
     # downloading the video
     d_video.download(SAVE_PATH)
